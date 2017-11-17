a4a2f8e1ca59e7443a22089fb483dbc741f1fe04
Bibek-Sutradhar <Bibek@198612>
2017-11-17 02:43:48 -0800

    updated pcf urls for production

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,5 @@
 export const environment = {
-  production: true
+  production: true,
+  userUrl: 'https://cf-user-mgmt-service.cfapps.io/users',
+    policyUrl: 'https://cf-policy-mgmt-service.cfapps.io/policies'
 };
